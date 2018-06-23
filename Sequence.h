b56d31de46634e73c4c90e16b65195ae303c667d
scut20173068 <964372370@qq.com>
2018-06-23 11:05:48 +0800

    头文件

---
+++ Sequence.h
@@ -0,0 +1,28 @@
+#ifndef SEQUENCE_H
+#define SEQUENCE_H
+
+class Sequence
+{
+
+Sequence(string);
+int length();
+int numberOf(char); 
+string longestConsecutive();
+string longestRepeated();
+string all;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
