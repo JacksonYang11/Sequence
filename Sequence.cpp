3523ae4ed2777a729288d3028ae16b5519183189
scut20173068 <964372370@qq.com>
2018-06-23 11:44:35 +0800

    头文件.cpp

--- Sequence.cpp
+++ Sequence.cpp
@@ -26,7 +26,16 @@ return length;
 
 int numberOf(char base)
 {
+int counter=0;
 
+char *p = all; 
+for(int i=0;i<all.strlen;i++)
+ {
+	 char c=p[i];
+	 if(c==base)
+	 {counter++;}
+ };
+return counter;
 }
 
 string longestConsecutive()
